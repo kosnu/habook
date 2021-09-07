e059eafd5d21114ca8fd2cf66e6a8e91e19046a5
Kosuke Numata <coursek8814@gmail.com>
2021-09-08 01:19:32 +0900

    :recycle: #7 AnchorElementの状態管理を共通のCustom Hooksで行うようにした

---
+++ frontend/src/ui/common/hooks/useAnchorElement.ts
@@ -0,0 +1,24 @@
+import React from "react"
+import { atomFamily, useRecoilState, useResetRecoilState } from "recoil"
+
+type AnchorElement = HTMLElement | null
+
+const anchorElementStateFamily = atomFamily<AnchorElement, string>({
+  key: "anchor-element-state-family",
+  default: null,
+})
+
+export function useAnchorElement(key: string) {
+  const [state, setState] = useRecoilState(anchorElementStateFamily(key))
+  const resetState = useResetRecoilState(anchorElementStateFamily(key))
+
+  function setAnchorElement(event: React.MouseEvent<HTMLButtonElement>) {
+    setState(event.currentTarget)
+  }
+
+  return {
+    anchorEl: state,
+    setAnchorElement: setAnchorElement,
+    resetAnchorElement: resetState,
+  }
+}
