3a805a3befe0a95bdeda9ecb2bc14241203c1a1f
Kosuke Numata <coursek8814@gmail.com>
2022-08-11 16:33:49 +0900

    :sparkles: DSN を環境変数から生成するようにした

--- backend/database/database.go
+++ backend/database/database.go
@@ -5,8 +5,7 @@ import (
 	"gorm.io/gorm"
 )
 
-func ConnectDatabase() (*gorm.DB, error) {
-	dsn := "habook:habook@tcp(db:3306)/habook?charset=utf8mb4&parseTime=True&loc=Local"
+func ConnectDatabase(dsn string) (*gorm.DB, error) {
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
