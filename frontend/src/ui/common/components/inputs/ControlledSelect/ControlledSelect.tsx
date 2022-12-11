894971a1b90658c164acc527f995c397b94efad5
Kosuke Numata <coursek8814@gmail.com>
2022-12-11 23:59:30 +0900

    :sparkles: React Hook Form 用の選択ボックスを作成した

---
+++ frontend/src/ui/common/components/inputs/ControlledSelect/ControlledSelect.tsx
@@ -0,0 +1,55 @@
+import {
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  Select,
+  SelectProps,
+} from "@mui/material"
+import { FormControlProps } from "@mui/material/FormControl/FormControl"
+import React, { ReactNode, useId } from "react"
+import { Control, useController } from "react-hook-form"
+import { FieldPath, FieldValues } from "react-hook-form/dist/types"
+
+interface ControlledSelectProps<
+  TFieldValues extends FieldValues,
+  TName extends FieldPath<TFieldValues>,
+> extends SelectProps {
+  name: TName
+  control: Control<TFieldValues>
+  label?: ReactNode
+  formControlProps?: FormControlProps
+}
+
+export function ControlledSelect<
+  TFieldValues extends FieldValues,
+  TName extends FieldPath<TFieldValues>,
+>({
+  control,
+  name,
+  label,
+  formControlProps,
+  children,
+  ...selectProps
+}: ControlledSelectProps<TFieldValues, TName>) {
+  const { field, fieldState } = useController({
+    control: control,
+    name: name,
+  })
+  const labelId = useId()
+
+  return (
+    <>
+      <FormControl
+        error={!!fieldState.error}
+        variant={"standard"}
+        {...formControlProps}
+      >
+        <InputLabel id={labelId}>{label}</InputLabel>
+        <Select labelId={labelId} {...selectProps} {...field}>
+          {children}
+        </Select>
+        <FormHelperText>{fieldState.error?.message}</FormHelperText>
+      </FormControl>
+    </>
+  )
+}
